f8098a0a5e83f459ab229fd8cf67c85338a88dcb
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-06 18:21:57 -0400

    refactor: Change Equality Overriding Implementation

--- HeightmapVisualizer/Units/Vector3.cs
+++ HeightmapVisualizer/Units/Vector3.cs
@@ -1,57 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Numerics;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeightmapVisualizer.Units
 {
-	public class Vector3
-	{
-		public static Vector3 Zero = new Vector3(0, 0, 0);
-		public static Vector3 Forward = new Vector3(0, 0, 1);
-		public static Vector3 Up = new Vector3(0, 1, 0);
-		public static Vector3 Right = new Vector3(1, 0, 0);
+    public class Vector3 : IEquatable<Vector3>
+    {
 
-		public float x {  get; set; }
-		public float y { get; set; }
-		public float z { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
 
-		public Vector3() : this(0, 0, 0) { }
+        public static Vector3 Zero = new Vector3(0, 0, 0);
+        public static Vector3 Forward = new Vector3(0, 0, 1);
+        public static Vector3 Up = new Vector3(0, 1, 0);
+        public static Vector3 Right = new Vector3(1, 0, 0);
 
-		public Vector3(float x, float y, float z)
-		{
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}
+        public Vector3() : this(0, 0, 0) { }
 
-		#region Operators
-
-		public static Vector3 operator+ (Vector3 v1, Vector3 v2)
-		{
-			return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-		}
-
-		public static Vector3 operator- (Vector3 v1, Vector3 v2)
-		{
-			return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-		}
-
-		public static Vector3 operator* (Vector3 v1, Vector3 v2)
-		{
-			return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-		}
-
-		public static Vector3 operator *(Vector3 v1, float s)
-		{
-			return new Vector3(v1.x * s, v1.y * s, v1.z * s);
-		}
-
-        public static Vector3 operator /(Vector3 v1, Vector3 v2)
+        public Vector3(float x, float y, float z)
         {
-            return new Vector3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        #region Operators
+
+        public static Vector3 operator +(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector3 operator -(Vector3 v1, Vector3 v2)
+        {
+            return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        public static Vector3 operator *(Vector3 v1, float s)
+        {
+            return new Vector3(v1.x * s, v1.y * s, v1.z * s);
         }
 
         public static Vector3 operator /(Vector3 v1, float s)
@@ -62,61 +49,72 @@ namespace HeightmapVisualizer.Units
         // Epsilon value for comparison
         private const float Epsilon = 0.01f;
 
-		// Method to compare two floats within an epsilon
-		private static bool AreApproximatelyEqual(float a, float b, float epsilon = Epsilon)
-		{
-			return Math.Abs(a - b) < epsilon;
-		}
+        // Method to compare two floats within an epsilon
+        private static bool AreApproximatelyEqual(float a, float b, float epsilon = Epsilon)
+        {
+            return Math.Abs(a - b) < epsilon;
+        }
 
-		// Override the == operator
-		public static bool operator ==(Vector3 v1, Vector3 v2)
-		{
-			if (ReferenceEquals(v1, v2))
-			{
-				return true;
-			}
+        // Override the == operator
+        public static bool operator ==(Vector3 v1, Vector3 v2)
+        {
+            if (ReferenceEquals(v1, v2))
+            {
+                return true;
+            }
 
-			if (v1 is null || v2 is null)
-			{
-				return false;
-			}
+            if (v1 is null || v2 is null)
+            {
+                return false;
+            }
 
-			// Compare each component using the epsilon
-			return AreApproximatelyEqual(v1.x, v2.x) &&
-				   AreApproximatelyEqual(v1.y, v2.y) &&
-				   AreApproximatelyEqual(v1.z, v2.z);
-		}
+            // Compare each component using the epsilon
+            return AreApproximatelyEqual(v1.x, v2.x) &&
+                   AreApproximatelyEqual(v1.y, v2.y) &&
+                   AreApproximatelyEqual(v1.z, v2.z);
+        }
 
-		// Override the != operator
-		public static bool operator !=(Vector3 v1, Vector3 v2)
-		{
-			return !(v1 == v2);
-		}
+        // Override the != operator
+        public static bool operator !=(Vector3 v1, Vector3 v2)
+        {
+            return !(v1 == v2);
+        }
 
-		// Override Equals method
-		public override bool Equals(object obj)
-		{
-			if (obj == null || GetType() != obj.GetType())
-			{
-				return false;
-			}
+        // Override Equals method
+        public override bool Equals(object? obj)
+        {
+            return obj is Vector3 other && Equals(other);
+        }
 
-			Vector3 v = (Vector3)obj;
-			return AreApproximatelyEqual(x, v.x) &&
-				   AreApproximatelyEqual(y, v.y) &&
-				   AreApproximatelyEqual(z, v.z);
-		}
+        /// <summary>
+        /// Checks if the current Vector3 is equal to another Vector3.
+        /// </summary>
+        /// <param name="other">The Vector3 to compare with the current Vector3.</param>
+        /// <returns>True if the specified Vector3 is equal to the current Vector3; otherwise, false.</returns>
+        public bool Equals(Vector3? other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
 
-		// Override GetHashCode method (hashcode may not reflect epsilon logic)
-		public override int GetHashCode()
-		{
-			return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
-		}
+            if (ReferenceEquals(this, other))
+                return true;
 
-		public override string ToString()
-		{
-			return $"({x}, {y}, {z})";
-		}
+            return AreApproximatelyEqual(x, other.x) &&
+                   AreApproximatelyEqual(y, other.y) &&
+                   AreApproximatelyEqual(z, other.z);
+        }
+
+        // Override GetHashCode method
+        public override int GetHashCode()
+        {
+            // Note: Consider documenting that hashcode doesn't account for epsilon precision.
+            return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y}, {z})";
+        }
 
         #endregion
 
