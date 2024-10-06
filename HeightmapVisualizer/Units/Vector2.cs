0bfe10705c230ee7622b19a8a9ca55174f83aadd
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-06 19:59:56 -0400

    feat: Add Division Operator Override on Vector2 Class
    Added Vector2 / Vector2 using Component-Wise Division

--- HeightmapVisualizer/Units/Vector2.cs
+++ HeightmapVisualizer/Units/Vector2.cs
@@ -44,6 +44,11 @@ namespace HeightmapVisualizer.Units
             return new Vector2(v1.x * x, v1.y * x);
         }
 
+        public static Vector2 operator /(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x / v2.x, v1.y / v2.y);
+        }
+
         public static Vector2 operator /(Vector2 v1, float x)
         {
             return new Vector2(v1.x / x, v1.y / x);
