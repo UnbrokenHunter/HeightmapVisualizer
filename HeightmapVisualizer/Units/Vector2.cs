79a29740f0cda332c4844094f759aab70a0ac2af
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-06 18:20:52 -0400

    feat: Add Plus and Minus Operator Overloads
    For a Vector2 + float or Vector2 - float

--- HeightmapVisualizer/Units/Vector2.cs
+++ HeightmapVisualizer/Units/Vector2.cs
@@ -1,38 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace HeightmapVisualizer.Units
 {
-	public class Vector2
-	{
-		public float x { get; set; }
-		public float y { get; set; }
+    public class Vector2
+    {
+        public float x { get; set; }
+        public float y { get; set; }
 
-		public Vector2() : this(0, 0) { }
+        public Vector2() : this(0, 0) { }
 
-		public Vector2(float x, float y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
-		public static Vector2 operator +(Vector2 v1, Vector2 v2)
-		{
-			return new Vector2(v1.x + v2.x, v1.y + v2.y);
-		}
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x + v2.x, v1.y + v2.y);
+        }
 
-		public static Vector2 operator -(Vector2 v1, Vector2 v2)
-		{
-			return new Vector2(v1.x - v2.x, v1.y - v2.y);
-		}
+        public static Vector2 operator +(Vector2 v1, float x)
+        {
+            return new Vector2(v1.x + x, v1.y + x);
+        }
 
-		public static Vector2 operator *(Vector2 v1, Vector2 v2)
-		{
-			return new Vector2(v1.x * v2.x, v1.y * v2.y);
-		}
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, float x)
+        {
+            return new Vector2(v1.x - x, v1.y - x);
+        }
+
+        public static Vector2 operator *(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x * v2.x, v1.y * v2.y);
+        }
 
         public static Vector2 operator *(Vector2 v1, float x)
         {
@@ -45,9 +50,9 @@ namespace HeightmapVisualizer.Units
         }
 
         public override string ToString()
-		{
-			return $"({x}, {y})";
-		}
+        {
+            return $"({x}, {y})";
+        }
 
-	}
+    }
 }
