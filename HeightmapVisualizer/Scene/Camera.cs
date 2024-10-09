fbb815102f4caf4ef9ed47791bcb395264cf2d9b
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-09 11:43:32 -0400

    refactor: Changed Camera Axis Orientation to Debug Mode

--- HeightmapVisualizer/Scene/Camera.cs
+++ HeightmapVisualizer/Scene/Camera.cs
@@ -51,14 +51,19 @@ namespace HeightmapVisualizer.Scene
 
         public override Mesh? GetRenderable()
         {
-            var combined = MeshUtility.CombineGeometry(
-                MeshUtility.CombineGeometry(
-                    Line.CreateRay(Transform.Position, Transform.Forward, 5, Color.CornflowerBlue),
-                    Line.CreateRay(Transform.Position, Transform.Up, 5, Color.Green)
-                ),
-                Line.CreateRay(Transform.Position, Transform.Right, 5, Color.Red)
-            );
-            return combined;
+            var debug = false;
+            if (debug)
+            {
+                var combined = MeshUtility.CombineGeometry(
+                    MeshUtility.CombineGeometry(
+                        Line.CreateRay(Transform.Position, Transform.Forward, 5, Color.CornflowerBlue),
+                        Line.CreateRay(Transform.Position, Transform.Up, 5, Color.Green)
+                    ),
+                    Line.CreateRay(Transform.Position, Transform.Right, 5, Color.Red)
+                );
+                return combined;
+            }
+            return null;
         }
     }
 }
