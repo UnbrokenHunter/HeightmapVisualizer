28aa80c48ac542d2502c68507ccef1cb6dcf154a
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-09 12:25:01 -0400

    refactor: Removed Unnecessary Buttons

--- HeightmapVisualizer/Menu.cs
+++ HeightmapVisualizer/Menu.cs
@@ -5,33 +5,19 @@ namespace HeightmapVisualizer
 {
     internal class Menu
     {
-        private UI.Button Pos;
-        private UI.Button Quat;
-        private UI.Button NQuat;
-        private UI.Button Euler;
-        private UI.Button fov;
+        private UI.Button PositionText;
+        private UI.Button EulerRotationText;
 
         public Menu()
         {
-            Pos = new UI.Button("Position", new Vector2(0, 60), new Vector2(100, 60), (button) => { });
-
-            Quat = new UI.Button("Quaterion", new Vector2(0, 240), new Vector2(100, 60), (button) => { });
-            NQuat = new UI.Button("Normalized Quaterion", new Vector2(0, 300), new Vector2(100, 60), (button) => { });
-            Euler = new UI.Button("Euler Angles", new Vector2(0, 360), new Vector2(100, 60), (button) => { });
-
-            fov = new UI.Button("FOV", new Vector2(0, 440), new Vector2(100, 60), (button) => { });
-
+            PositionText = new UI.Button("Position", new Vector2(0, 60), new Vector2(200, 60));
+            EulerRotationText = new UI.Button("Euler Angles", new Vector2(0, 360), new Vector2(200, 60));
         }
 
         public void Update(Camera cam)
         {
-            Pos.text = $"Position:{cam.Transform.Position}";
-
-            Quat.text = $"Quaterion: {cam.Transform.Rotation}";
-            NQuat.text = $"Normalized Quaterion: {Quaternion.Normalize(cam.Transform.Rotation)}";
-            Euler.text = $"Euler Angles:{Quaternion.ToPitchYawRoll(cam.Transform.Rotation)}";
-
-            fov.text = $"FOV:{cam.Fov}";
+            PositionText.SetText($"Position:{cam.Transform.Position}");
+            EulerRotationText.SetText($"Euler Angles:{Quaternion.ToPitchYawRoll(cam.Transform.Rotation)}");
         }
     }
 }
