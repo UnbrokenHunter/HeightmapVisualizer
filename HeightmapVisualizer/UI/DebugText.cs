c9f0b2cf396ef98a1c84614348048c0ba7ff86f4
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-09 12:25:59 -0400

    refactor: Replaced Text with Debug Text
    Can be used to create text on screen without creating an object

--- HeightmapVisualizer/UI/DebugText.cs
+++ HeightmapVisualizer/UI/DebugText.cs
@@ -3,35 +3,32 @@ using HeightmapVisualizer.Units;
 
 namespace HeightmapVisualizer.UI
 {
-	class Text
+	class DebugText
 	{
-		private static List<Text> texts = new List<Text>();
+		private static List<DebugText> texts = new List<DebugText>();
 
 		public string text;
 		public Vector2 position1;
-		public Vector2 position2;
 
 		// Constructor for the Button class
-		public Text(string text, Vector2 position, Vector2 size)
+		public DebugText(string text, Vector2 position)
 		{
 			texts.Add(this);
 
 			this.text = text;
 			position1 = position;
-			position2 = position1 + size;
 		}
 
 		public static void Draw(Graphics g)
 		{
-			Pen pen = new Pen(Color.Black);
 			Font font = new Font("Arial", 13f);
 			Brush brush = new SolidBrush(Color.Black);
 
-			foreach (Text b in texts)
+			foreach (DebugText b in texts)
 			{
-				g.DrawRectangle(pen, (int)b.position1.x, (int)b.position1.y, (int)b.position2.x, (int)b.position2.y);
 				g.DrawString(b.text, font, brush, b.position1.x, b.position1.y);
 			}
+			texts.Clear();
 		}
 	}
 }
