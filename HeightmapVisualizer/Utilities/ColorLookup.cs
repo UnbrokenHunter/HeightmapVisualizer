116ac579592728af6b738fc8ebe02edece927c11
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-10 11:59:50 -0400

    feat: Add Color Lookup Class

---
+++ HeightmapVisualizer/Utilities/ColorLookup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeightmapVisualizer.Utilities
+{
+	public static class ColorLookup
+	{
+		private static Dictionary<Color, Brush> brushes = new Dictionary<Color, Brush>();
+		public static Brush FindOrGetBrush(Color color)
+		{
+			brushes.TryGetValue(color, out Brush? value);
+
+			if (value == null)
+			{
+				value = new SolidBrush(color);
+				brushes.Add(color, value);
+			}
+
+			return value;
+		}
+
+		private static Dictionary<Color, Pen> pens = new Dictionary<Color, Pen>();
+		public static Pen FindOrGetPen(Color color)
+		{
+			pens.TryGetValue(color, out Pen? value);
+
+			if (value == null)
+			{
+				value = new Pen(color);
+				pens.Add(color, value);
+			}
+
+			return value;
+		}
+	}
+}
