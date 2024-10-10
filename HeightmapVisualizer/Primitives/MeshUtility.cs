0d7b0f59f192b65e8922bc747d0645aae05a29d9
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-10 12:02:47 -0400

    feat: Added Convert2DArrayTo1DArray Method

--- HeightmapVisualizer/Primitives/MeshUtility.cs
+++ HeightmapVisualizer/Primitives/MeshUtility.cs
@@ -5,13 +5,32 @@ namespace HeightmapVisualizer.Primitives
 {
     public static class MeshUtility
     {
-        /// <summary>
-        /// Combines two mesh Geometries into a single mesh object. 
-        /// </summary>
-        /// <param name="m1"></param>
-        /// <param name="m2"></param>
-        /// <returns>A Mesh Object with combined geometry</returns>
-        public static Mesh CombineGeometry(Mesh m1, Mesh m2)
+		/// <summary>
+		/// Converts a 2D array of objects into a 1D array of objects
+		/// </summary>
+		/// <param name="array">The 2D Array of objects to convert</param>
+		/// <returns></returns>
+		public static Mesh[] Convert2DArrayTo1DArray(Mesh[,] array)
+		{
+			var objs = new Mesh[array.GetLength(0) * array.GetLength(1)];
+			for (var i = 0; i < array.GetLength(0); i++)
+			{
+				for (var j = 0; j < array.GetLength(1); j++)
+				{
+					var index = array.GetLength(1) * i + j;
+					objs[index] = array[i, j];
+				}
+			}
+			return objs;
+		}
+
+		/// <summary>
+		/// Combines two mesh Geometries into a single mesh object. 
+		/// </summary>
+		/// <param name="m1"></param>
+		/// <param name="m2"></param>
+		/// <returns>A Mesh Object with combined geometry</returns>
+		public static Mesh CombineGeometry(Mesh m1, Mesh m2)
         {
             
             var faces1 = new List<Face>();
