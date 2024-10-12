9b5d51afbe8b149640d74f231fcf29ecb683da14
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-12 18:42:54 -0400

    feat: Add Renderable Class

---
+++ HeightmapVisualizer/Rendering/Renderable.cs
@@ -0,0 +1,19 @@
+﻿
+using HeightmapVisualizer.Units;
+
+namespace HeightmapVisualizer.Rendering
+{
+    public sealed class Renderable
+    {
+        public DrawingMode DrawingMode { get; private set; }
+        public Vector2[] Position { get; private set; }
+        public Color Color { get; private set; }
+
+        public Renderable(DrawingMode drawingMode, Vector2[] position, Color color)
+        {
+            DrawingMode = drawingMode;
+            Position = position;
+            Color = color;
+        }
+    }
+}
