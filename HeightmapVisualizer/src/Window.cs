8a4ebe4aab799593cc79ad27f1ba41ec7293fdd1
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-10-26 18:19:11 -0400

    feat: Add FPS Display to Screen

--- HeightmapVisualizer/src/Window.cs
+++ HeightmapVisualizer/src/Window.cs
@@ -102,13 +102,18 @@ namespace HeightmapVisualizer.src
 				((Button)g).SetText(Instance.Scene.Camera.Gameobject.Transform.Rotation.ToString());
 			}
 
+            static void updateFPS(UIElement g)
+            {
+                ((Button)g).SetText("FPS: " + Gameloop.Instance.FPS);
+            }
 
-			// CREATE MENU
-			UIElement[] ui = new List<UIElement>
+            // CREATE MENU
+            UIElement[] ui = new List<UIElement>
 			{
-				new Button(new Vector2(0, 0), new Vector2(400, 60), "Position", id: "pos", update: updatePos),
-				new Button(new Vector2(0, 60), new Vector2(400, 60), "Euler Angles", id: "ang", update: updateAng)
-			}.ToArray();
+                new Button(new Vector2(0, 0), new Vector2(400, 60), "Position", id: "pos", update: updatePos),
+                new Button(new Vector2(0, 60), new Vector2(400, 60), "Euler Angles", id: "ang", update: updateAng),
+                new Button(new Vector2(0, 120), new Vector2(400, 60), "FPS", id: "fps", update: updateFPS),
+            }.ToArray();
 
 			return new Scene.Scene(objects, ui);
         }
