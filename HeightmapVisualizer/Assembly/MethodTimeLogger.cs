9304603a819b50560868c842e4b763982aa66299
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-11-02 16:02:04 -0400

    feat: Add Foly Method Timer

---
+++ HeightmapVisualizer/Assembly/MethodTimeLogger.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Diagnostics;
+using System.Reflection;
+
+namespace HeightmapVisualizer.Assembly
+{
+    public static class MethodTimeLogger
+    {
+        public static void Log(MethodBase method, TimeSpan timeSpan)
+        {
+            Trace.WriteLine("Method: " + method.Name + " Time: " + timeSpan.TotalMilliseconds);
+        }
+    }
+}
