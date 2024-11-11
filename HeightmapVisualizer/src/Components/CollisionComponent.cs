af71f3ff5cfd694a728b75a7c88c297105e18a45
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-11-11 17:13:05 -0500

    feat: Add Empty Collision Component

---
+++ HeightmapVisualizer/src/Components/CollisionComponent.cs
@@ -0,0 +1,18 @@
+﻿
+using HeightmapVisualizer.src.Scene;
+
+namespace HeightmapVisualizer.src.Components
+{
+	internal class CollisionComponent : Component
+	{
+		public override void Init(Gameobject gameobject)
+		{
+			base.Init(gameobject);
+		}
+
+		public override void Update()
+		{
+			
+		}
+	}
+}
