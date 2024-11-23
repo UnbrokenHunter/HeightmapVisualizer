ca033d7b815d91b589f961d5301291b7fd53bf3d
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-11-22 19:17:47 -0500

    feat: Add Constant Gravity Module

---
+++ HeightmapVisualizer/src/Components/Physics/Gravity/ConstantGravityPhysicsModule.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Numerics;
+
+namespace HeightmapVisualizer.src.Components.Physics.Gravity
+{
+    internal class ConstantGravityPhysicsModule : GravityPhysicsModule
+    {
+        internal Vector3 GravityVector { get; private set; }
+        internal ConstantGravityPhysicsModule SetGravityVector(Vector3 gravityVector)
+        {
+            this.GravityVector = gravityVector;
+            return this;
+        }
+
+        internal ConstantGravityPhysicsModule() 
+        {
+            GravityVector = new Vector3 (0, 9.81f, 0);
+        }
+
+        internal override void Gravity(PhysicsComponent physics)
+        {
+            physics.SetVelocity (physics.Velocity + (GravityVector * (float)Gameloop.Instance.DeltaTime));
+        }
+    }
+}
